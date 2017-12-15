ecf499810f72c53543cb05d72b1cfc4f13770c85
Robin Janssens <robin@robinjanssens.net>
2017-12-15 13:01:37 +0100

    improved column selecter

--- opgave3.m
+++ opgave3.m
@@ -45,139 +45,149 @@ function update(handles)
     global output;          % make 'output' variable accesable
     global fs;              % make 'fs' variable accesable
 
-    column = str2double(get(handles.edit_column, 'String'));
-    columns = size(xls_in,2);
-    if 1 <= column && column <= columns
+    % ------------------------------
+    % Select Column
+    % ------------------------------
+    column = str2double(get(handles.edit_column, 'String'));	% read column textbox
+    column = uint32(column);                                    % make it unsigned integer to remove negative numbers and decimal numbers
+    columns = size(xls_in,2);                                   % get the amount of columns in 'xls_in'
+    if column < 1                                               % if selected column is smaller than first existing column
+        column = 1;                                             % select first column
+    elseif column > columns                                     % if selected column is larger than amount of existing columns
+        column = columns;                                       % select last column
+    end
+    set(handles.edit_column,'string',num2str(column));          % change value in textbox
+    input = xls_in(:,column);                                   % select column from 'xls_in'
 
-        input = xls_in(:,column); % select column
+    % ------------------------------
+    % x-axis values
+    % ------------------------------
+    n = 0:1:length(input)-1;                      % generate sample x-axis values
+    f = fs*(0:(length(input)/2))/length(input);   % generate frequency x-axis values
 
-        n = 0:1:length(input)-1;                    % generate sample x-axis values
-        f = fs*(0:(length(input)/2))/length(input); % generate frequency x-axis values
+    % input FFT
+    input_fft = fft(input);                                 % calculate fft from the input data
+    input_fft = abs(input_fft);                             % take the absolute value of the vector to eliminate phase
+    input_fft = input_fft/fs;                               % devide by sampling frequency to get the right amplitudes
+    input_fft = input_fft(1:floor(length(input)/2+1));      % only take upper band
 
-        % input FFT
-        input_fft = fft(input);                                 % calculate fft from the input data
-        input_fft = abs(input_fft);                             % take the absolute value of the vector to eliminate phase
-        input_fft = input_fft/fs;                               % devide by sampling frequency to get the right amplitudes
-        input_fft = input_fft(1:floor(length(input)/2+1));      % only take upper band
+    % ------------------------------
+    % Window Function
+    % ------------------------------
+    contents = cellstr(get(handles.menu_window,'String'));          % get menu content
+    selected_window = contents(get(handles.menu_window,'Value'));   % get menu value
+    if strcmp(selected_window,'No Window')
+        window = rectwin(length(input));
+    elseif strcmp(selected_window,'Bartlett')
+        window = bartlett(length(input));
+    elseif strcmp(selected_window,'Chebyshev')
+        window = chebwin(length(input));
+    elseif strcmp(selected_window,'Hamming')
+        window = hamming(length(input));
+    elseif strcmp(selected_window,'Hann')
+        window = hann(length(input));
+    end
 
-        % ------------------------------
-        % Window Function
-        % ------------------------------
-        contents = cellstr(get(handles.menu_window,'String'));          % get menu content
-        selected_window = contents(get(handles.menu_window,'Value'));   % get menu value
-        if strcmp(selected_window,'No Window')
-            window = rectwin(length(input));
-        elseif strcmp(selected_window,'Bartlett')
-            window = bartlett(length(input));
-        elseif strcmp(selected_window,'Chebyshev')
-            window = chebwin(length(input));
-        elseif strcmp(selected_window,'Hamming')
-            window = hamming(length(input));
-        elseif strcmp(selected_window,'Hann')
-            window = hann(length(input));
+    % window FFT
+    window_fft = fft(window);                               % calculate fft from the window data
+    window_fft = abs(window_fft);                           % take the absolute value of the vector to eliminate phase
+    window_fft = window_fft/fs;                             % devide by sampling frequency to get the right amplitudes
+    window_fft = window_fft(1:floor(length(input)/2+1));    % only take upper band
+
+
+    % calculate output
+    output = input .* window;
+
+    % ------------------------------
+    % Filter
+    % ------------------------------
+    span = str2double(get(handles.edit_span,'String'));           % read span textbox
+    span = uint32(span);                                          % make it unsigned integer to remove negative numbers and decimal numbers
+    set(handles.edit_span,'string',num2str(span));                % change value in textbox
+
+    contents = cellstr(get(handles.menu_filter,'String'));        % get popchoice content
+    selected_filter = contents(get(handles.menu_filter,'Value')); % get popchoice value
+    if strcmp(selected_filter,'Moving Average') || strcmp(selected_filter,'Savitzky-Golay Filter')
+        % in 'Moving Average' mode and 'Savitzky-Golay' mode span needs to be bigger than 1 and an odd number
+        if mod(span,2) == 0                                       % if span is not odd
+            span = span-1;                                        % make it odd
+            set(handles.edit_span,'string',num2str(span));        % change value in textbox
         end
-
-        % window FFT
-        window_fft = fft(window);                               % calculate fft from the window data
-        window_fft = abs(window_fft);                           % take the absolute value of the vector to eliminate phase
-        window_fft = window_fft/fs;                             % devide by sampling frequency to get the right amplitudes
-        window_fft = window_fft(1:floor(length(input)/2+1));    % only take upper band
-        
-
-        % calculate output
-        output = input .* window;
-
-        % ------------------------------
-        % Filter
-        % ------------------------------
-        span = str2double(get(handles.edit_span,'String'));           % read span textbox
-        span = uint32(span);                                          % make it unsigned integer
-        set(handles.edit_span,'string',num2str(span));                % change value in textbox
-
-        contents = cellstr(get(handles.menu_filter,'String'));        % get popchoice content
-        selected_filter = contents(get(handles.menu_filter,'Value')); % get popchoice value
-        if strcmp(selected_filter,'Moving Average') || strcmp(selected_filter,'Savitzky-Golay Filter')
-            % in 'Moving Average' mode and 'Savitzky-Golay' mode span needs to be bigger than 1 and an odd number
-            if mod(span,2) == 0                                       % if span is not odd
-                span = span-1;                                        % make it odd
-                set(handles.edit_span,'string',num2str(span));        % change value in textbox
-            end
-            if span < 1                                               % if span is smaller than 1
-                span = 1;                                             % make it 1
-                set(handles.edit_span,'string',num2str(span));        % change value in textbox
-            end
-        elseif strcmp(selected_filter,'Local Regression (1th degree)') || strcmp(selected_filter,'Local Regression (2de degree)') || strcmp(selected_filter,'Robust Local Regression (1th degree)') || strcmp(selected_filter,'Robust Local Regression (2de degree)')
-            % in regression modes span needs to be a percentage between 0 and 99
-            if span > 99                                              % if span is bigger than 99
-                span = 99;                                            % make it 99
-                set(handles.edit_span,'string',num2str(span));        % change value in textbox
-            end
+        if span < 1                                               % if span is smaller than 1
+            span = 1;                                             % make it 1
+            set(handles.edit_span,'string',num2str(span));        % change value in textbox
         end
-
-
-
-        degree = str2double(get(handles.edit_degree,'String')); % read degree textbox
-        degree = uint32(degree);                                % make it unsigned integer
-        set(handles.edit_degree,'string',num2str(degree));      % change value in textbox
-        if degree > span-1                                      % if degree is larger than span-1
-            degree = span-1;                                    % make it span-1
-            set(handles.edit_degree,'string',num2str(degree));  % change value in textbox
+    elseif strcmp(selected_filter,'Local Regression (1th degree)') || strcmp(selected_filter,'Local Regression (2de degree)') || strcmp(selected_filter,'Robust Local Regression (1th degree)') || strcmp(selected_filter,'Robust Local Regression (2de degree)')
+        % in regression modes span needs to be a percentage between 0 and 99
+        if span > 99                                              % if span is bigger than 99
+            span = 99;                                            % make it 99
+            set(handles.edit_span,'string',num2str(span));        % change value in textbox
         end
-
-        if strcmp(selected_filter,'No Filter')                                    % if no filter is selected
-                                                                            % output = output (do nothing)
-        elseif strcmp(selected_filter,'Moving Average')                           % if filter 'Moving Average' is selected
-            output = smooth(output,double(span),'moving');                  % perform smooth() on output (span in samples)
-        elseif strcmp(selected_filter,'Local Regression (1th degree)')            % if filter 'Local Regression (1th degree)' is selected
-            output = smooth(output,double(span)/100,'lowess');              % perform smooth() on output (span in percentage)
-        elseif strcmp(selected_filter,'Local Regression (2de degree)')            % if filter 'Local Regression (2de degree)' is selected
-            output = smooth(output,double(span)/100,'loess');               % perform smooth() on output (span in percentage)
-        elseif strcmp(selected_filter,'Savitzky-Golay Filter')                    % if filter Savitzky-Golay Filter' is selected
-            output = smooth(output,double(span),'sgolay',double(degree));   % perform smooth() on output (span in samples)
-        elseif strcmp(selected_filter,'Robust Local Regression (1th degree)')     % if filter 'Robust Local Regression (1th degree)' is selected
-            output = smooth(output,double(span)/100,'rlowess');             % perform smooth() on output (span in percentage)
-        elseif strcmp(selected_filter,'Robust Local Regression (2de degree)')     % if filter 'Robust Local Regression (2de degree)' is selected
-            output = smooth(output,double(span)/100,'rloess');              % perform smooth() on output (span in percentage)
-        end
+    end
 
 
 
+    degree = str2double(get(handles.edit_degree,'String')); % read degree textbox
+    degree = uint32(degree);                                % make it unsigned integer to remove negative numbers and decimal numbers
+    set(handles.edit_degree,'string',num2str(degree));      % change value in textbox
+    if degree > span-1                                      % if degree is larger than span-1
+        degree = span-1;                                    % make it span-1
+        set(handles.edit_degree,'string',num2str(degree));  % change value in textbox
+    end
 
-        % output FFT
-        output_fft = fft(output);                               % calculate fft from the output data
-        output_fft = abs(output_fft);                           % take the absolute value of the vector to eliminate phase
-        output_fft = output_fft/fs;                             % devide by sampling frequency to get the right amplitudes
-        output_fft = output_fft(1:floor(length(input)/2+1));    % only take upper band
+    if strcmp(selected_filter,'No Filter')                                    % if no filter is selected
+                                                                        % output = output (do nothing)
+    elseif strcmp(selected_filter,'Moving Average')                           % if filter 'Moving Average' is selected
+        output = smooth(output,double(span),'moving');                  % perform smooth() on output (span in samples)
+    elseif strcmp(selected_filter,'Local Regression (1th degree)')            % if filter 'Local Regression (1th degree)' is selected
+        output = smooth(output,double(span)/100,'lowess');              % perform smooth() on output (span in percentage)
+    elseif strcmp(selected_filter,'Local Regression (2de degree)')            % if filter 'Local Regression (2de degree)' is selected
+        output = smooth(output,double(span)/100,'loess');               % perform smooth() on output (span in percentage)
+    elseif strcmp(selected_filter,'Savitzky-Golay Filter')                    % if filter Savitzky-Golay Filter' is selected
+        output = smooth(output,double(span),'sgolay',double(degree));   % perform smooth() on output (span in samples)
+    elseif strcmp(selected_filter,'Robust Local Regression (1th degree)')     % if filter 'Robust Local Regression (1th degree)' is selected
+        output = smooth(output,double(span)/100,'rlowess');             % perform smooth() on output (span in percentage)
+    elseif strcmp(selected_filter,'Robust Local Regression (2de degree)')     % if filter 'Robust Local Regression (2de degree)' is selected
+        output = smooth(output,double(span)/100,'rloess');              % perform smooth() on output (span in percentage)
+    end
 
-        % plot input
-        axes(handles.plot_input);
-        if get(handles.checkbox_input,'Value') == 0 % 0 => time / 1 => frequency
-            plot(n,input);
-            xlabel('n (sample)');
-        else
-            stem(f,input_fft);
-            xlabel('f (Hz)');
-        end
 
-        % plot window function
-        axes(handles.plot_window);
-        if get(handles.checkbox_window,'Value') == 0 % 0 => time / 1 => frequency
-            plot(n,window);
-            xlabel('n (sample)');
-        else
-            stem(f,window_fft);
-            xlabel('f (Hz)');
-        end
 
-        % plot output
-        axes(handles.plot_output);
-        if get(handles.checkbox_output,'Value') == 0 % 0 => time / 1 => frequency
-            plot(n,real(output));
-            xlabel('n (sample)');
-        else
-            stem(f,output_fft);
-            xlabel('f (Hz)');
-        end
+
+    % output FFT
+    output_fft = fft(output);                               % calculate fft from the output data
+    output_fft = abs(output_fft);                           % take the absolute value of the vector to eliminate phase
+    output_fft = output_fft/fs;                             % devide by sampling frequency to get the right amplitudes
+    output_fft = output_fft(1:floor(length(input)/2+1));    % only take upper band
+
+    % plot input
+    axes(handles.plot_input);
+    if get(handles.checkbox_input,'Value') == 0 % 0 => time / 1 => frequency
+        plot(n,input);
+        xlabel('n (sample)');
+    else
+        stem(f,input_fft);
+        xlabel('f (Hz)');
+    end
+
+    % plot window function
+    axes(handles.plot_window);
+    if get(handles.checkbox_window,'Value') == 0 % 0 => time / 1 => frequency
+        plot(n,window);
+        xlabel('n (sample)');
+    else
+        stem(f,window_fft);
+        xlabel('f (Hz)');
+    end
+
+    % plot output
+    axes(handles.plot_output);
+    if get(handles.checkbox_output,'Value') == 0 % 0 => time / 1 => frequency
+        plot(n,real(output));
+        xlabel('n (sample)');
+    else
+        stem(f,output_fft);
+        xlabel('f (Hz)');
     end
 end
 
